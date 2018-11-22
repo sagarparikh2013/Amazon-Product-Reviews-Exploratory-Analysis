7cbbd47b0e6878138359500c58cf320dc9b420ea
asavaliy <asavaliy@sfu.ca>
2018-11-21 22:34:19 -0800

    Final for One dataset

--- wordcloud_try.py
+++ wordcloud_try.py
@@ -42,43 +42,34 @@ def main(inputs,start_year,end_year):
 
 	input_df = spark.read.parquet(inputs).cache()
 	input_df.registerTempTable("input_df")
-	input_df.show()
-
 	get_reviews= spark.sql("SELECT review_body FROM input_df")
 	text_review=get_reviews.collect()
 
 	stopwords = set(STOPWORDS)
-	stopwords.update(["take","Row","review_body","br","quot","although","now","say","well","new","made","much","make","got","each","take","use","may","without","part","want","makes","even","many","used","yet","going","set","come","go","found","another","seem"])
-
+	stopwords.update(["take","look","put","around","Row","review_body","br","quot","although","now","say","well","new","made","much","make","got","each","take","use","may","without","part","want","makes","even","many","used","yet","going","set","come","go","found","another","seem"])
 	stopwords.update(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"])
-
-	
-
-	# transformed_cloud_mask = np.ndarray((cloud_mask.shape[0],cloud_mask.shape[1],cloud_mask.shape[2]), np.int32)
-
-	# for i in range(len(cloud_mask)):
-	# 	transformed_cloud_mask[i] = list(map(transform_format, cloud_mask[i]))
 
 	#Add Details for Cloud
 	font_path="fonts/forbes_bold.ttf"
-	cloud_mask = np.array(Image.open("cloud.png"))
-	#Canvas Width
-	width=1000
-	height=800
-	max_words=75
-	background_color="black"
-	contour_width=40
-	contour_color="white"
+	image_name="cloud4.png"
+	cloud_mask = np.array(Image.open(image_name))
+	width=1500
+	height=1000
+	max_words=150
+	background_color="white"
+	contour_width=0
+	min_font_size=4
+	contour_color="black"
+	relative_scaling=0
+	mode="RGB"
+	save_as_name="img/"+image_name
 
-
-
-
-	wordcloud = WordCloud(font_path=font_path,mask=cloud_mask,width=width,height=height,stopwords=stopwords, max_words=max_words, background_color=background_color,contour_width=contour_width,contour_color=contour_color).generate(str(text_review))
+	wordcloud = WordCloud(relative_scaling=relative_scaling,min_font_size=min_font_size,mode=mode,font_path=font_path,mask=cloud_mask,width=width,height=height,stopwords=stopwords, max_words=max_words, background_color=background_color,contour_width=contour_width,contour_color=contour_color).generate(str(text_review))
 	plt.figure()
 	plt.imshow(wordcloud, interpolation="bilinear")
 	plt.axis("off")
 	plt.show()
-	wordcloud.to_file("img/first_review.png")
+	wordcloud.to_file(save_as_name)
 
 
 # def transform_format(val):
