ee4647c002686855cc1a2406e93121635845bc94
asavaliy <asavaliy@sfu.ca>
2018-11-21 21:37:58 -0800

    Updated Stopwords

--- wordcloud_try.py
+++ wordcloud_try.py
@@ -52,14 +52,18 @@ def main(inputs,start_year,end_year):
 	print(get_reviews.count())
 	text_review=get_reviews.collect()
 
-	
-	wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="black").generate(str(text_review))
+	stopwords = set(STOPWORDS)
+	stopwords.update(["take","Row","review_body","br","quot","although","now","say","well","new","made","much","make","got","each","take","use","may","without","part","want","makes","even","many","used","yet","going","set","come","go","found","another","seem"])
+
+	stopwords.update(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"])
+
+
+	wordcloud = WordCloud(font_path="fonts/forbes_bold.ttf",width=1000,height=800,stopwords=stopwords, max_words=75, background_color="black").generate(str(text_review))
 	plt.figure()
 	plt.imshow(wordcloud, interpolation="bilinear")
 	plt.axis("off")
 	plt.show()
 	wordcloud.to_file("img/first_review.png")
-
 
 if __name__ == '__main__':
 	inputs = sys.argv[1]
