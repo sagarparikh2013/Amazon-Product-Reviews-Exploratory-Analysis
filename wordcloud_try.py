869c09e8f5f9bec4dd0a989a505c34675b4c21f9
asavaliy <asavaliy@sfu.ca>
2018-11-21 21:49:59 -0800

    With Image

--- wordcloud_try.py
+++ wordcloud_try.py
@@ -44,9 +44,6 @@ def main(inputs,start_year,end_year):
 	input_df.registerTempTable("input_df")
 	input_df.show()
 
-
-
-
 	get_reviews= spark.sql("SELECT review_body FROM input_df")
 	get_reviews.show()
 	print(get_reviews.count())
@@ -57,13 +54,27 @@ def main(inputs,start_year,end_year):
 
 	stopwords.update(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"])
 
+	cloud_mask = np.array(Image.open("cloud.png"))
 
-	wordcloud = WordCloud(font_path="fonts/forbes_bold.ttf",width=1000,height=800,stopwords=stopwords, max_words=75, background_color="black").generate(str(text_review))
+	# transformed_cloud_mask = np.ndarray((cloud_mask.shape[0],cloud_mask.shape[1],cloud_mask.shape[2]), np.int32)
+
+	# for i in range(len(cloud_mask)):
+	# 	transformed_cloud_mask[i] = list(map(transform_format, cloud_mask[i]))
+
+
+	wordcloud = WordCloud(font_path="fonts/forbes_bold.ttf",mask=cloud_mask,width=1000,height=800,stopwords=stopwords, max_words=75, background_color="black",contour_width=15,contour_color="white").generate(str(text_review))
 	plt.figure()
 	plt.imshow(wordcloud, interpolation="bilinear")
 	plt.axis("off")
 	plt.show()
 	wordcloud.to_file("img/first_review.png")
+
+
+# def transform_format(val):
+# 	if val.all() == 0:
+# 		return 255
+# 	else:
+# 		return val
 
 if __name__ == '__main__':
 	inputs = sys.argv[1]
